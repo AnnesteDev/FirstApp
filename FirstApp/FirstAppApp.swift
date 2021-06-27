aed833d9b0aedfc0d4d152403ebf0ff7aa40689a
Steven Grobler <stepdev@icloud.com>
2021-06-27 20:52:10 +0200

    Initial Commit

---
+++ FirstApp/FirstAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FirstAppApp.swift
+//  FirstApp
+//
+//  Created by Steven on 27/06/2021.
+//
+
+import SwiftUI
+
+@main
+struct FirstAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
